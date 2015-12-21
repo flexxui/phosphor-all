d702066bb83212b41edebbb4508bd68ad4813d8f
Almar Klein <almar.klein@gmail.com>
2015-12-21 13:32:32 +0100

    style: avoid flake8 errors when used as a subdir in flexx repo

--- run.py
+++ run.py
@@ -38,7 +38,9 @@ package = """
 CODE = """
 window.phosphor.createWidget = function (name) {
     var ori = phosphor.widget.Widget.createNode;
-    phosphor.widget.Widget.createNode = function() {return document.createElement(name);};
+    phosphor.widget.Widget.createNode = function() {
+        return document.createElement(name);
+    };
     var w = new phosphor.widget.Widget();
     phosphor.widget.Widget.createNode = ori;
     return w;
@@ -62,7 +64,7 @@ check_call(['npm', 'install'])
 # Create index.js
 code = ''
 # for m in modules:
-    # code += 'var %s = require("phosphor-%s");\n' % (m, m)
+#     code += 'var %s = require("phosphor-%s");\n' % (m, m)
 code += 'window.phosphor = {};\n'
 for m in modules:
     code += 'window.phosphor.%s = require("phosphor-%s");\n' % (m, m)
@@ -71,7 +73,8 @@ open('index.js', 'wt').write(code)
 
 # Create bundle
 check_call(['npm', 'install', 'browserify', 'browserify-css', 'uglify'])
-check_call(['browserify', '-g', '[', 'browserify-css', '--minify=true', ']', 'index.js', '-o', 'phosphor-all.js'])
+check_call(['browserify', '-g', '[', 'browserify-css', '--minify=true', ']', 
+            'index.js', '-o', 'phosphor-all.js'])
 # check_call(['uglify', '-s', 'phosphor-all.js', '-o', 'phosphor-all.min.js'])
 # todo: uglify is broken on windows?
 
