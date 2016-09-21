da2aa9a609f5514a1fc080c86b282fd3120a0d82
Almar Klein <almar.klein@gmail.com>
2016-09-21 10:55:55 +0200

    Include css, and use webpack to bundle

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ browserify and uglify.
 """
 
 import subprocess
-import json
 import sys
 import os
 
@@ -13,7 +12,7 @@ from flexx.util.minify import minify
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(THIS_DIR)
-        
+
 modules = 'core', 'dom', 'ui', 'collections', 'algorithm'
 
 package = """
@@ -23,31 +22,19 @@ package = """
   "description": "Phosphor in one JS file.",
   "main": "index.js",
   "scripts": {
-    "test": "echo \\"Error: no test specified\\" && exit 1"
+    "test": "echo \\"Error: no test specified\\" && exit 1",
+    "bundle": "webpack index.js phosphor-all.js"
   },
   "author": "Almar Klein",
   "license": "BSD-3-Clause",
   "repository": "https://github.com/zoofIO/phosphor-all",
-  "dependencies": {"phosphor": "latest"}
+  "dependencies": {"phosphor": "latest"},
+  "devDependencies": {"webpack": "latest"}
 }
 """.strip()
 
-# todo: make a createPanel function
-
-CODE = """
-window.phosphor.createWidget = function (name) {
-    var ori = phosphor.ui.widget.Widget.createNode;
-    phosphor.ui.widget.Widget.createNode = function() {
-        return document.createElement(name);
-    };
-    var w = new phosphor.ui.widget.Widget();
-    phosphor.ui.widget.Widget.createNode = ori;
-    return w;
-};
-"""
 
 LICENSE = '/*\n%s\n*/\n' % open('phosphor_license.txt', 'rb').read().decode().strip()
-
 
 def check_call(cmd, **kwargs):
     kwargs['cwd'] = THIS_DIR
@@ -65,7 +52,7 @@ check_call(['npm', 'install'])
 code = 'window.phosphor = {};\n'
 phosphor_dir = os.path.join(THIS_DIR, 'node_modules', 'phosphor', 'lib')
 for subpackage_name in modules:
-    code += 'window.phosphor.%s = {};\n' %  subpackage_name
+    code += 'window.phosphor.%s = {};\n' % subpackage_name
     for module_name in os.listdir(os.path.join(phosphor_dir, subpackage_name)):
         if not module_name.endswith('.js'):
             continue
@@ -73,20 +60,23 @@ for subpackage_name in modules:
         t = 'window.phosphor.%s.%s = require("phosphor/lib/%s/%s");\n'
         code += t % (subpackage_name, module_name, subpackage_name, module_name)
 
-code += CODE
 open('index.js', 'wt').write(code)
 
 # Create bundle
-check_call(['npm', 'install', 'browserify', 'browserify-css', 'uglify'])
-check_call(['browserify', '-g', '[', 'browserify-css', '--minify=true', ']', 
-            'index.js', '-o', 'phosphor-all.js'])
+check_call(['npm', 'run', 'bundle'])
 
 # Uglify breaks if its run from any other drive than C:
 # I only got it from 290 to 210 so I decided not to go through the trouble
 #check_call(['uglify', '-o', 'phosphor-all.min.js', 'phosphor-all.js'])
 
-# Strip comments in non-minified, and add license text once
+# Perform our own lightweight minification, and add license text once
 text = open('phosphor-all.js', 'rt').read()
 text = minify(text, False)
 text = LICENSE + text
 open('phosphor-all.js', 'wt').write(text)
+
+# Copy minified and licence-added version of Phosphor's base.css
+text = open('node_modules/phosphor/styles/base.css', 'rt').read()
+text = minify(text, False)
+text = LICENSE + text
+open('phosphor-all.css', 'wt').write(text)
