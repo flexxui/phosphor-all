b875b3893cac2d33735d37b9e19b695c3fbec6ee
Almar Klein <almar.klein@gmail.com>
2016-11-28 16:27:33 +0100

    update run script

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 """
-This script defines a selection of phosphorjs modules, and uses npm to
-install and bundle them in one standalone js file. Needs npm with
-browserify and uglify.
+This script defines a selection of phosphorjs modules, and uses npm and
+webpack to install and bundle them in one standalone js file.
 """
 
 import os
 import sys
+import json
 import subprocess
 import os.path as op
 
@@ -24,10 +24,10 @@ for d in (WORK_DIR, DIST_DIR):
 
 MODULES = 'core', 'dom', 'ui', 'collections', 'algorithm'
 
-package = """
+PACKAGE = """
 {
   "name": "phosphor-all",
-  "version": "1.0.0",
+  "version": "1.1.0",
   "description": "Phosphor in one JS file.",
   "main": "index.js",
   "scripts": {
@@ -35,15 +35,40 @@ package = """
     "bundle": "webpack index.js phosphor-all.js"
   },
   "author": "Almar Klein",
-  "license": "BSD-3-Clause",
+  "license": "BSD-2-Clause",
   "repository": "https://github.com/zoofIO/phosphor-all",
   "dependencies": {"phosphor": "latest"},
   "devDependencies": {"webpack": "latest"}
 }
 """.strip()
 
+INDEX = """
+var phosphor_version = "PHOSPHOR_VERSION";  // easy to find line that defines version
+function require_phosphor (path) {
+    // Normalize input
+    if (path.endsWith('.js')) { path = path.slice(0, -3); }
+    if (path.startsWith('/')) { path = path.slice(1); }
+    if (path.startsWith('phosphor/')) { path = path.slice(9); }
+    // Return module or throw error
+    var mod = modules[path];
+    if (typeof mod === 'undefined') {
+        throw "Phosphor lib '" + path + "' is not present in Phosphor-all bundle.";
+    }
+    return mod;
+}
+require_phosphor.phosphor_version = phosphor_version;
+window.require_phosphor = require_phosphor;
+
+var modules = {};
+MAPPINGS
+"""
+
 
 ## Utils
+
+def read(filename):
+    with open(filename, 'rt') as file:
+        return file.read()
 
 def write(filename, text):
     with open(filename, 'wt') as file:
@@ -59,18 +84,21 @@ def check_call(cmd, **kwargs):
 
 
 def create_index():
-    # Create index.js
-    code = 'window.phosphor = {};\n'
+    code = INDEX
+    # Set version
+    fname = op.join(WORK_DIR, 'node_modules', 'phosphor', 'package.json')
+    code = code.replace('PHOSPHOR_VERSION', json.load(open(fname))['version'])
+    # Get mappings
+    mappings = []
     phosphor_dir = op.join(WORK_DIR, 'node_modules', 'phosphor', 'lib')
     for subpackage_name in MODULES:
-        code += 'window.phosphor.%s = {};\n' % subpackage_name
         for module_name in os.listdir(op.join(phosphor_dir, subpackage_name)):
             if not module_name.endswith('.js'):
                 continue
-            module_name = module_name.split('.')[0]
-            t = 'window.phosphor.%s.%s = require("phosphor/lib/%s/%s");\n'
-            code += t % (subpackage_name, module_name, subpackage_name, module_name)
-    return code
+            path = subpackage_name + '/' + module_name.split('.')[0]
+            t = 'modules["lib/%s"] = require("phosphor/lib/%s");'
+            mappings.append(t % (path, path))
+    return code.replace('MAPPINGS', '\n'.join(mappings))
 
 
 def css_prefixer(text):
@@ -96,32 +124,35 @@ def css_prefixer(text):
 
 ## Run npm
 
-# Write package spec so npm knows our dependencies
-write(op.join(WORK_DIR, 'package.json'), package)
-
-# Install/update dependencies
-check_call(['npm', 'install'])
-
-# Write our index file (needs previous step)
-write(op.join(WORK_DIR, 'index.js'), create_index())
-
-# Create bundle
-check_call(['npm', 'run', 'bundle'])
+if True:  # Turn this off to easily tweak result without rebuilding
+    
+    # Write package spec so npm knows our dependencies
+    write(op.join(WORK_DIR, 'package.json'), PACKAGE)
+    
+    # Install/update dependencies
+    check_call(['npm', 'install'])
+    
+    # Write our index file (needs previous step)
+    write(op.join(WORK_DIR, 'index.js'), create_index())
+    
+    # Create bundle
+    check_call(['npm', 'run', 'bundle'])
 
 
 ## Produce dist result
 
 # Get sources
-phosphor_js = open(op.join(WORK_DIR, 'phosphor-all.js'), 'rt').read()
-phosphor_css = open(op.join(WORK_DIR, 'node_modules/phosphor/styles/base.css'), 'rt').read()
-phosphor_license = open(op.join(WORK_DIR, 'node_modules/phosphor/LICENSE'), 'rt').read()
+phosphor_js = read(op.join(WORK_DIR, 'phosphor-all.js'))
+phosphor_css = read(op.join(WORK_DIR, 'node_modules/phosphor/styles/base.css'))
+phosphor_license = read(op.join(WORK_DIR, 'node_modules/phosphor/LICENSE'))
+phosphor_license = '/*\n%s\n*/\n' % phosphor_license.strip()
 
 # Process JS asset
 js = phosphor_license + minify(phosphor_js, False)
 write(op.join(DIST_DIR, 'phosphor-all.js'), js)
 
 # Process CSS asset
-css = phosphor_css + open(op.join(THIS_DIR, 'more_phosphor.css'), 'rt').read()
+css = phosphor_css + read(op.join(THIS_DIR, 'more_phosphor.css'))
 css = css_prefixer(minify(css, False))
 css = "/* Phosphor CSS, prefixed for Flexx */\n\n" + phosphor_license + css
 write(op.join(DIST_DIR, 'phosphor-all.css'), css)
